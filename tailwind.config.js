a7bf77eebab6736d064ff96335793366a1f2e165
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2024-04-02 08:53:48 -0400

    add empty pages and tailwind cover

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./layouts/**/*.html', './content/**/*.md'],
+  content: [
+    './layouts/**/*.html', 
+    './content/**/*.md', 
+    './content/**/*.html'
+  ],
   theme: {
     extend: {},
   },
-  plugins: [    
+  plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
-  ],  
+  ],
 }
 
